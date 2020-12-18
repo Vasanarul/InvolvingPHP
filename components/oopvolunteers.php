07d540f89995d913da104dfcf7b25ec712017d52
Vasan Arul <vasankarul@gmail.com>
2020-12-17 20:52:21 -0800

    Addition of Inherited VolunteerDriver class

--- components/oopvolunteers.php
+++ components/oopvolunteers.php
@@ -29,5 +29,35 @@
   echo "$Dennis->name<br>";
   echo "$Dennis->work<br>";
   echo $Dennis->getHours();
+  echo "<br><br>";
+
+  // class VolunteerDriver extends Volunteer {
+  //   public $carBrand;
+
+  //   public function __construct($name, $work, $hours, $carBrand) {
+  //     parent::__construct($name, $work, $hours);
+  //     $this->carBrand = $carBrand;
+  //   }
+
+  //   function getHours(){
+  //     return $this->hours;
+  //   }
+
+  //   function setHours($hours){
+  //     if(is_int($hours) == true || is_float($hours) == true) {
+  //       $this->hours = $hours;
+  //     } else {
+  //       $this->hours = "Error: Invalid datatype.";
+  //     }
+  //   }
+
+  // }
+
+  // $AD = new VolunteerDriver("Anthony Davis", "Habitat for Humanity", 24, "Shelby GT Mustang");
+
+  // echo "$AD->name<br>";
+  // echo "$AD->work<br>";
+  // echo $AD->getHours();
+  // echo"<br>$AD->carBrand";
 
 ?>
